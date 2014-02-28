28690ff339ddc12fdfc5fc1747f1a41e8a0d8917
moosemaniam <vikasmk@gmail.com>
2014-03-01 00:28:49 +0530

    Changed how a character moves. Using target.

--- main.py
+++ main.py
@@ -29,6 +29,10 @@ class platform(pygame.sprite.Sprite):
    
 class player(pygame.sprite.Sprite):
     offset = 2
+    xtarget=0
+    ytarget=0
+    xspeed=0
+    yspeed=0
     def __init__(self,left,top,width,height,color):
        pygame.sprite.Sprite.__init__(self)
        self.rect = pygame.Rect(left,top,width,height) 
@@ -37,14 +41,41 @@ class player(pygame.sprite.Sprite):
        self.color = color
        self.x = left
        self.y =top 
+       self.xtarget = self.x
+       self.ytarget = self.y
+
+    def print_info(self):
+        print "x" ,self.x
+        print "y" ,self.y
+        print "xtarget",self.xtarget
+        print "ytarget",self.ytarget
     def update(self):
-        pass
+        print "update"
+        if(self.xtarget>self.x):
+            self.move_right()
+        if(self.xtarget<self.x):
+            self.move_left()
+        if(self.ytarget<self.y):
+            self.move_up()
+        if(self.ytarget>self.y):
+            self.move_down()
+
+    def move_down(self):
+       print "moving down"
+       self.y = self.y + 1
+       self.rect.move_ip(0,1)
+    def move_up(self):
+       print "moving up"
+       self.y = self.y - 1
+       self.rect.move_ip(0,-1)
 
     def move_left(self):
+       print "moving left"
        self.x = self.x - 1
        self.rect.move_ip(-1*self.offset,0)
 
     def move_right(self):
+       print "moving right"
        self.x = self.x + 1
        self.rect.move_ip(self.offset,0)
 
@@ -59,7 +90,6 @@ class Game(object):
         clock1 = pygame.time.Clock()
         screen.fill(BLACK)
         screen_rect = pygame.Rect(0,0,screen.get_width(),screen.get_height())
-
         all_Sprite_list = pygame.sprite.Group()
         player_Sprite= pygame.sprite.Group()
    #def __init__(self,left,top,width,height,color):
@@ -83,9 +113,17 @@ class Game(object):
                     return
                     
                 if ((event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT)):
-                    joe.move_right()
+                        joe.xtarget = joe.x + 10
                 if ((event.type == pygame.KEYDOWN) and (event.key == pygame.K_LEFT)):
-                    joe.move_left()
+                        joe.xtarget = joe.x - 10
+
+                if ((event.type == pygame.KEYDOWN) and (event.key == pygame.K_UP)):
+                        joe.ytarget = joe.y - 5 
+                if ((event.type == pygame.KEYDOWN) and (event.key == pygame.K_DOWN)):
+                        joe.ytarget = joe.y + 5 
+
+           # joe.print_info()
+            joe.update()
             all_Sprite_list.update() 
             all_Sprite_list.draw(screen) 
             joe.draw(screen) 
@@ -111,6 +149,6 @@ w=400
 h=600
 pygame.display.set_caption("Moose Pong")
 screen = pygame.display.set_mode((w,h))
-pygame.key.set_repeat(10,10)
+pygame.key.set_repeat(1000/FPS,1000/FPS)
 jump_up= Game()
 jump_up.main(screen)
